86ab44fba5331635464bb4fce3f7d74ccdd10ce6
hwan001 <woghks7209@gmail.com>
2023-01-02 23:32:24 +0900

    pluto 웹 분리 - front
    signup 테스트 성공

---
+++ pluto_front/static/js/function.js
@@ -0,0 +1,31 @@
+function draw_graph(arr_data, tickers_len, tickers){
+    for (var i=0; i < tickers_len; i++){
+        var chart = new Chart(document.getElementById("TSLA").getContext('2d'), {
+        type: 'line',
+        data: {
+            labels: ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12'],
+            datasets: [{
+            label: "Closing price",
+            backgroundColor: 'transparent',
+            borderColor: 'aliceblue',
+            data: arr_data[i],
+            }]
+        },
+        options: {
+            scales: {
+	    		yAxes: [{
+	    			ticks: {
+	    				stepSize : 1,
+	    				fontSize : 12,
+	    			}
+	    		}]
+	    	}
+        }
+        });
+    }
+}
+
+function signup(a, b){
+    const name = document.getElementById('name').value;
+    document.getElementById("result").innerText = name;
+}
